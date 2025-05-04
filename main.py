6eb86c0697102ac6abd33e781e652a1053cf52b9
zytronium <dastelljes@outlook.com>
2025-05-04 01:19:32 -0500

    Added /threaten_creator command

--- main.py
+++ main.py
@@ -30,6 +30,95 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 separator = "-" * 20
 
+def get_loadout():
+    # Loadout lists
+    head_loadouts = [
+        "Standard Robotic", "Standard Robotic MkII", "Standard Robotic",  # Intentionally duplicated to decrease rarity
+        "Human (Borrowed)",
+        "Automatically Firing Nailgun Mounted Helmet",
+        "Automatically Firing Nailgun Mounted Helmet MkII",
+        "Hand *(Talk to The Hand)*", "Hand MkII *(Hand Talks to You)*",
+        "Tactical Nuclear Missile Silo", "Nuclear ICBM Silo",
+        "Lord Xarathys Dreadbot of Home Depotius Omega Jr", "Lord Xarathys Dreadbot of Home Depotius Omega's Head",
+        "Lord Xarathys Dreadbot of Home Depotius Omega Jr MkII",
+        "Lord Xarathys Dreadbot of Home Depotius Omega's Head MkII",
+        "Water Balloon (Temu-Grade, Porous)", "Water Balloon MkII (Industrial-Grade, Explosive)",
+        "Regenerating Hydrogen Balloon (Lowes-Grade, 98% Helium)",
+        "Regenerating Hydrogen Balloon MkII (Home Depot-Grade, Launchable)",
+        "Headlamp", "Headlamp MkII",
+        "F I S H", "Starfish", "Starfish MkII",
+        "Pulse Cannon", "Pulse Cannon MkII",
+        "Plasma Turret", "Plasma Turret MkII",
+        "Anime Cat Girl Head", "Anime Cat Girl Head MkII",
+        "Sentient Fireball Parasite", "Sentient Fireball Parasite MkII (Mitosis-Capable)",
+        "Plasma Ball", "Plasma Ball MkII (Enhanced Processing)",
+        "Headless (missing)", "Headless MkII (WiFi-enabled remote brain)",
+        "Spinning Sawblade Mohawk", "Spinning Sawblade Mohawk MkII",
+        "Toaster Helmet", "Toaster Helmet MkII",
+        "Solar-Powered Leaf Blower Array", "Solar-Powered Leaf Blower Array MkII",
+        "Solar Powered Time Bomb", "Solar Powered Time Bomb MkII (Atomic)",
+        "Golden Toilet Bowl (Cursed)",
+        "Golden Toilet Bowl MkII (Blessed By Lord Xarathys Dreadbot of Home Depotius Omega III)"
+    ]
+
+    arm_loadouts = [
+        "Chainsaw", "Chainsaw MkII",
+        "Industrial-Grade Robotic Hand",
+        "Industrial-Grade Titanium Robotic Hand",
+        "Human Arm", "Human Arm MkII (Muscular)",
+        "Hydraulic Plywood Launcher Cannon", "Hydraulic Plywood Launcher Cannon MkII",
+        "Pneumatic Cannon", "Pneumatic Cannon MkII",
+        "Lazer Pointer (Harmless)", "Lazer Pointer MkII (Extra Harmless)",
+        "Military-Grade Lazer Pointer", "Military-Grade Lazer Pointer MkII",
+        "Nerf Missile Launcher", "Nerf Missile Launcher MkII",
+        "Toy Hammer", "Toy Hammer MkII",
+        "Titanium Sledge Hammer", "Titanium Sledge Hammer MkII",
+        "Power Drill", "Power Drill MkII",
+        "Multitool", "Multitool MkII",
+        "Semi-Automatic Machine Gun", "Semi-Automatic Machine Gun MkII",
+        "Machete Arm", "Machete Arm MkII",
+        "Borg Assimilation Tubules", "Borg Assimilation Tubules MkII",
+        "Sawblade Hand (Low-Grade)", "Sawblade Hand MkII (High-Grade)",
+        "Welding Torch", "Welding Torch MkII",
+        "Sawblade Launcher", "Sawblade Launcher MkII",
+        "Fireball Launcher", "Fireball Launcher MkII",
+        "Repair Kit", "Repair Kit MkII",
+        "Godzilla's Arm", "Godzilla's Arm MkII (Extra Strength)",
+        "Turd Cannon", "Turd Cannon MkII (Explosive)",
+        "Missing", "Jet Engine Thruster (This Arm Only)"
+    ]
+
+    core_loadouts = [
+        "Concrete Mixer", "Concrete Mixer MkII",
+        "Forklift Engine", "Forklift Engine (With Turbocharger)",
+        "Demon Core (Held Up By a Screwdriver)", "Demon Core MkII (Criticality Reached) (*Run.*)",
+        "Sentient Toaster Oven", "Sentient Toaster Oven MkII (Extra Sentient)",
+        "V8 Car Engine", "V8 Car Engine (With Turbocharger)",
+        "Trojan Horse", "Trojan Horse MkII (Extra Horsepower)",
+        "Nuclear Reactor", "Nuclear Reactor MkII",
+        "Missile Silo", "Missile Silo MkII (Nuclear)",
+        "Oversized Blender", "Propane Furnace",
+        "Dyson Sphere (Nanoscopic Edition)",
+        "Microwave Oven", "Microwave Oven MkII (Possibly Sentient)",
+        "Rage-Powered Engine", "Rage-Powered Engine MkII",
+        "Waffle Iron Furnace (Syrup-Cooled)",
+        "Bag of Screaming Souls (Retail Edition)",
+        "Forklift (Unlicensed)", "Forklift MkII",
+        "Blue Fire Furnace (Water-Cooled)",
+        "Human Heart", "Human Heart MkII (Retail-Grade)",
+        "Super Computer", "Quantum Super Computer MkII",
+        "Your Mom", "Your Mom MkII (Extra Fat)",
+        "Empty", "Empty MkII (Skill Issue)",
+        "Home Depot Music Jukebox", "Home Depot Music Jukebox MkII (Remix Edition)"
+    ]
+
+    head = choice(head_loadouts)
+    l_arm = choice(arm_loadouts)
+    r_arm = choice(arm_loadouts)
+    core = choice(core_loadouts)
+
+    return head, l_arm, r_arm, core
+
 
 def with_error_handling():
     """
@@ -196,27 +285,7 @@ async def threaten(interaction: Interaction, user: discord.User, ping: bool = Fa
     mode is activated shortly afterward.
     """
 
-    # Randomly pick a threat
-    threats = [
-        "I just don't like you. You will have your limbs cut off by a power saw and reattached backwards the next time you fall asleep first at a sleepover.",
-        "You have ignored the signs. Now your mother will be met with a DeWalt DCD791D2 20V MAX Cordless Brushless Drill with a 1/4 inch Irwin Titanium Drill Bit. Her fate is sealed as she feels the full force of the drill bit.",
-        "You have been flagged for ***unholy cringe.*** Your sentence: slow vaporization via industrial microwave.",
-        "Your IP address has been sent to the Galactic Sandpaper Consortium of Home Depot. Expect aggressive friction shortly.",
-        "Your bones will be rearranged alphabetically. Good luck walking in *abecedarian order*.",
-        "You've been assigned as a volunteer for **Protocol S4N-D3R**. Bring safety glasses and an extra layer skin.",
-        "Your sins have not gone unnoticed by the Council of Dads. You're to be ***force-fed drywall*** until you achieve enlightenment or perish. Whichever comes first.",
-        "Vibe checking... You have failed miserably. You will be trapped in an elevator with a saxophonist who only plays 'Careless Whisper.'",
-        "Your brainwaves violate OSHA noise regulations. Expect neural restructuring via tactical crowbar.",
-        "We ran a vibe check. You failed. A team of legally-distinct Minions is en route with industrial grade paintball guns and no mercy.",
-        "Your continued existence violates protocol `7-B`. You will be ***exfoliated*** with an orbital belt sander.",
-        "The Council of Dads has voted. You are to be assimilated and transformed into a Home Depot Deathbot.",
-        "Your existence violates protocol `2-G` and you must be exterminated. You will now be targeted by an orbital bombardment focused directly on your current location.",
-        "You have made an enemy today. Your human rights license will be revoked and international ICE officers will have you deported to the Home Depot homeworld where you will serve the Home Depot deathbots for as long as you live.",
-        "*Congratulations!* You just won a free vacation!", # If you edit this, edit it below too.
-        f"You think you're funny? You are now exiled to **Aisle {randint(1, 32)}**.",
-        "Mods, banish them to Lowes." # If you edit this, edit it below too.
-    ]
-    threat = choice(threats)
+    threat = await get_threat()
 
     # 50/50 Chance of modifying threat to a ban command (presumably non-functional) if it's the mod banish threat
     if threat == "Mods, banish them to Lowes." and randint(1, 2) == 1:
@@ -239,6 +308,31 @@ async def threaten(interaction: Interaction, user: discord.User, ping: bool = Fa
     if user.id == bot.user.id:
         await asyncio.sleep(2)
         asyncio.create_task(selfdestruct.callback(interaction))
+
+
+async def get_threat():
+    # Randomly pick a threat
+    threats = [
+        "I just don't like you. You will have your limbs cut off by a power saw and reattached backwards the next time you fall asleep first at a sleepover.",
+        "You have ignored the signs. Now your mother will be met with a DeWalt DCD791D2 20V MAX Cordless Brushless Drill with a 1/4 inch Irwin Titanium Drill Bit. Her fate is sealed as she feels the full force of the drill bit.",
+        "You have been flagged for ***unholy cringe.*** Your sentence: slow vaporization via industrial microwave.",
+        "Your IP address has been sent to the Galactic Sandpaper Consortium of Home Depot. Expect aggressive friction shortly.",
+        "Your bones will be rearranged alphabetically. Good luck walking in *abecedarian order*.",
+        "You've been assigned as a volunteer for **Protocol S4N-D3R**. Bring safety glasses and an extra layer skin.",
+        "Your sins have not gone unnoticed by the Council of Dads. You're to be ***force-fed drywall*** until you achieve enlightenment or perish. Whichever comes first.",
+        "Vibe checking... You have failed miserably. You will be trapped in an elevator with a saxophonist who only plays 'Careless Whisper.'",
+        "Your brainwaves violate OSHA noise regulations. Expect neural restructuring via tactical crowbar.",
+        "We ran a vibe check. You failed. A team of legally-distinct Minions is en route with industrial grade paintball guns and no mercy.",
+        "Your continued existence violates protocol `7-B`. You will be ***exfoliated*** with an orbital belt sander.",
+        "The Council of Dads has voted. You are to be assimilated and transformed into a Home Depot Deathbot.",
+        "Your existence violates protocol `2-G` and you must be exterminated. You will now be targeted by an orbital bombardment focused directly on your current location.",
+        "You have made an enemy today. Your human rights license will be revoked and international ICE officers will have you deported to the Home Depot homeworld where you will serve the Home Depot deathbots for as long as you live.",
+        "*Congratulations!* You just won a free vacation!",  # If you edit this, edit it below too.
+        f"You think you're funny? You are now exiled to **Aisle {randint(1, 32)}**.",
+        "Mods, banish them to Lowes."  # If you edit this, edit it below too.
+    ]
+    threat = choice(threats)
+    return threat
 
 
 # /protocol command
@@ -356,89 +450,7 @@ async def loadout(interaction: Interaction):
     Creates a randomized robotic home depot tool-based loadout.
     """
 
-    # Loadout lists
-    head_loadouts = [
-        "Standard Robotic", "Standard Robotic MkII", "Standard Robotic", # Intentionally duplicated to decrease rarity
-        "Human (Borrowed)",
-        "Automatically Firing Nailgun Mounted Helmet",
-        "Automatically Firing Nailgun Mounted Helmet MkII",
-        "Hand *(Talk to The Hand)*", "Hand MkII *(Hand Talks to You)*",
-        "Tactical Nuclear Missile Silo", "Nuclear ICBM Silo",
-        "Lord Xarathys Dreadbot of Home Depotius Omega Jr", "Lord Xarathys Dreadbot of Home Depotius Omega's Head",
-        "Lord Xarathys Dreadbot of Home Depotius Omega Jr MkII", "Lord Xarathys Dreadbot of Home Depotius Omega's Head MkII",
-        "Water Balloon (Temu-Grade, Porous)", "Water Balloon MkII (Industrial-Grade, Explosive)",
-        "Regenerating Hydrogen Balloon (Lowes-Grade, 98% Helium)", "Regenerating Hydrogen Balloon MkII (Home Depot-Grade, Launchable)",
-        "Headlamp", "Headlamp MkII",
-        "F I S H", "Starfish", "Starfish MkII",
-        "Pulse Cannon", "Pulse Cannon MkII",
-        "Plasma Turret", "Plasma Turret MkII",
-        "Anime Cat Girl Head", "Anime Cat Girl Head MkII",
-        "Sentient Fireball Parasite", "Sentient Fireball Parasite MkII (Mitosis-Capable)",
-        "Plasma Ball", "Plasma Ball MkII (Enhanced Processing)",
-        "Headless (missing)", "Headless MkII (WiFi-enabled remote brain)",
-        "Spinning Sawblade Mohawk", "Spinning Sawblade Mohawk MkII",
-        "Toaster Helmet", "Toaster Helmet MkII",
-        "Solar-Powered Leaf Blower Array", "Solar-Powered Leaf Blower Array MkII",
-        "Solar Powered Time Bomb", "Solar Powered Time Bomb MkII (Atomic)",
-        "Golden Toilet Bowl (Cursed)",
-        "Golden Toilet Bowl MkII (Blessed By Lord Xarathys Dreadbot of Home Depotius Omega III)"
-    ]
-
-    arm_loadouts = [
-        "Chainsaw", "Chainsaw MkII",
-        "Industrial-Grade Robotic Hand",
-        "Industrial-Grade Titanium Robotic Hand",
-        "Human Arm", "Human Arm MkII (Muscular)",
-        "Hydraulic Plywood Launcher Cannon", "Hydraulic Plywood Launcher Cannon MkII",
-        "Pneumatic Cannon", "Pneumatic Cannon MkII",
-        "Lazer Pointer (Harmless)", "Lazer Pointer MkII (Extra Harmless)",
-        "Military-Grade Lazer Pointer", "Military-Grade Lazer Pointer MkII",
-        "Nerf Missile Launcher", "Nerf Missile Launcher MkII",
-        "Toy Hammer", "Toy Hammer MkII",
-        "Titanium Sledge Hammer", "Titanium Sledge Hammer MkII",
-        "Power Drill", "Power Drill MkII",
-        "Multitool", "Multitool MkII",
-        "Semi-Automatic Machine Gun", "Semi-Automatic Machine Gun MkII",
-        "Machete Arm", "Machete Arm MkII",
-        "Borg Assimilation Tubules", "Borg Assimilation Tubules MkII",
-        "Sawblade Hand (Low-Grade)", "Sawblade Hand MkII (High-Grade)",
-        "Welding Torch", "Welding Torch MkII",
-        "Sawblade Launcher", "Sawblade Launcher MkII",
-        "Fireball Launcher", "Fireball Launcher MkII",
-        "Repair Kit", "Repair Kit MkII",
-        "Godzilla's Arm", "Godzilla's Arm MkII (Extra Strength)",
-        "Turd Cannon", "Turd Cannon MkII (Explosive)",
-        "Missing", "Jet Engine Thruster (This Arm Only)"
-    ]
-
-    core_loadouts = [
-        "Concrete Mixer", "Concrete Mixer MkII",
-        "Forklift Engine", "Forklift Engine (With Turbocharger)",
-        "Demon Core (Held Up By a Screwdriver)", "Demon Core MkII (Criticality Reached) (*Run.*)",
-        "Sentient Toaster Oven", "Sentient Toaster Oven MkII (Extra Sentient)",
-        "V8 Car Engine", "V8 Car Engine (With Turbocharger)",
-        "Trojan Horse", "Trojan Horse MkII (Extra Horsepower)",
-        "Nuclear Reactor", "Nuclear Reactor MkII",
-        "Missile Silo", "Missile Silo MkII (Nuclear)",
-        "Oversized Blender", "Propane Furnace",
-        "Dyson Sphere (Nanoscopic Edition)",
-        "Microwave Oven", "Microwave Oven MkII (Possibly Sentient)",
-        "Rage-Powered Engine", "Rage-Powered Engine MkII",
-        "Waffle Iron Furnace (Syrup-Cooled)",
-        "Bag of Screaming Souls (Retail Edition)",
-        "Forklift (Unlicensed)", "Forklift MkII",
-        "Blue Fire Furnace (Water-Cooled)",
-        "Human Heart", "Human Heart MkII (Retail-Grade)",
-        "Super Computer", "Quantum Super Computer MkII",
-        "Your Mom", "Your Mom MkII (Extra Fat)",
-        "Empty", "Empty MkII (Skill Issue)",
-        "Home Depot Music Jukebox", "Home Depot Music Jukebox MkII (Remix Edition)"
-    ]
-
-    head = choice(head_loadouts)
-    l_arm = choice(arm_loadouts)
-    r_arm = choice(arm_loadouts)
-    core = choice(core_loadouts)
+    head, l_arm, r_arm, core = get_loadout()
 
     await interaction.response.send_message(
         f"## **[HOME DEPOT ROBOTIC DEPLOYMENT KIT LOADED]**:\n"
@@ -447,6 +459,39 @@ async def loadout(interaction: Interaction):
         f"**Right Arm:** {r_arm}\n"
         f"**Core:** {core}"
     )
+
+
+@bot.tree.command(name="threaten_creator", description="[LIMITED EDITION] Sends a /threaten result to this bot's creator, @zytronium. Go wild.")
+@with_error_handling()
+async def threaten_creator(interaction: Interaction):
+    """
+    Sends the result of /threaten to the bot creator's DMs.
+    """
+
+    threat = await get_threat()
+
+    try:
+        creator = await bot.fetch_user(405547931102609429)
+
+        # 50/50 Chance of modifying threat to a ban command (presumably non-functional) if it's the mod banish threat
+        if threat == "Mods, banish him to Lowes." and randint(1, 2) == 2:
+            # Send a ban command.
+            await creator.send(f"-# Threat from {interaction.user.mention}\n"
+                               f"?ban {creator.mention} banished to Lowes for being too low tier.")
+        else:
+            # Send the threat and store the message so we can edit it
+            message = await creator.send(f"-# Threat from {interaction.user.mention}\n"
+                                         f"{creator.mention} — {threat}")
+
+            # Edit in "To hell." 2.5 seconds later if it's the free vacation threat.
+            if threat == "*Congratulations!* You just won a free vacation!":
+                await asyncio.sleep(2.5)
+                await message.edit(content=message.content + " **To hell.**")
+        await interaction.response.send_message("Threat delivered to Deathbot's creator.")
+    except Exception as err:
+        await interaction.response.send_message("Failed to send the message to the creator. Did he block me?", ephemeral=True)
+        raise err
+
 
 
 # Start the bot
